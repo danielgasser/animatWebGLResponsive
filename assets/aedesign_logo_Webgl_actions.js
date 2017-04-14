19b9392f6c0786900708187d731da40dc3d2eef9
Daniel Gasser <software@toesslab.ch>
2017-04-14 11:54:59 +0200

    1. Commit

---
+++ assets/aedesign_logo_Webgl_actions.js
@@ -0,0 +1,9 @@
+var flwebgl;
+(function (flwebgl) {
+	(function (actions) {
+		actions.sc_Scene321_0_99 = function() {
+			stop();
+
+		}
+	})(flwebgl.actions || (flwebgl.actions = {}));
+})(flwebgl || (flwebgl = {}));
